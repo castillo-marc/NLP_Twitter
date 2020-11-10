810487ee370af7f57d95ebdc38794b599580a239
Marc <marc.castillo@berkeley.edu>
2020-11-10 17:00:47 -0500

    updated to incorporate txt output of snscrape

--- pull_tweets.py
+++ pull_tweets.py
@@ -53,3 +53,37 @@ columns = ["id",
 
 tweet_df = pd.DataFrame(data=data, columns=columns)
 pd.to_datetime(tweet_df["date_time"]).describe()
+
+# after tesla.txt is constructed:
+
+tweet_url = pd.read_csv("tesla.txt", index_col=None, header=None, names=["links"])
+af = lambda x: x["links"].split("/")[-1]
+tweet_url["id"] = tweet_url.apply(af, axis=1)
+
+ids = tweet_url["id"].tolist()
+
+# still need to instantiate api 
+
+# this function from:
+# https://medium.com/@jcldinco/downloading-historical-tweets-using-tweet-ids-via-snscrape-and-tweepy-5f4ecbf19032
+def fetch_tw(ids):
+    list_of_tw_status = api.statuses_lookup(ids, tweet_mode= "extended")
+    empty_data = pd.DataFrame()
+    for status in list_of_tw_status:
+            tweet_elem = {"id": status.id,
+                     "user": status.user.screen_name,
+                     "text":status.full_text,
+                     "date_time":status.created_at}
+            empty_data = empty_data.append(tweet_elem, ignore_index = True)
+    empty_data.to_csv("tesla_tweets.csv", mode="a")
+    
+    
+# process 50 entries at a time, bc original author couldn't loop through literally everything
+
+total_count = len(ids)
+chunks = (total_count - 1) // 50 + 1
+
+for i in range(chunks):
+    batch = ids[i*50:(i+1)*50]
+    result = fetch_tw(batch)
+    
