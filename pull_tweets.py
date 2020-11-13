189323a0691876743b983c65dacc0ada777c3896
Marc <marc.castillo@berkeley.edu>
2020-11-13 15:42:51 -0500

    constructed tesla_csv again

--- pull_tweets.py
+++ pull_tweets.py
@@ -19,9 +19,10 @@ access_secret = os.environ.get("ACCESS_SECRET")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-
+"""
+# SCRATCH WORK; IGNORE THIS
 # query = "Tesla OR Elon"
 # tweets = tweepy.Cursor(api.search, q=query,
 #                        tweet_mode="extended",
@@ -54,19 +55,25 @@ api = tweepy.API(auth)
 # tweet_df = pd.DataFrame(data=data, columns=columns)
 # pd.to_datetime(tweet_df["date_time"]).describe()
 
-# after tesla.txt is constructed:
+"""
 
+# below constructs list of urls to tweets satistfying query; requires snscrape
+# takes a long time to run so comment out after running once
+# cmd = "snscrape twitter-search \"#Tesla since:2020-01-01 until:2020-10-31\" > tesla.txt"
+# os.system(cmd)
+
+# after tesla.txt is constructed:
 tweet_url = pd.read_csv("tesla.txt", index_col=None, header=None, names=["links"])
 af = lambda x: x["links"].split("/")[-1]
 tweet_url["id"] = tweet_url.apply(af, axis=1)
 
 ids = tweet_url["id"].tolist()
 
-# still need to instantiate api 
-
 # this function from:
 # https://medium.com/@jcldinco/downloading-historical-tweets-using-tweet-ids-via-snscrape-and-tweepy-5f4ecbf19032
+# but also modified it a little so that the column names weren't made each time
 def fetch_tw(ids):
+    import os
     list_of_tw_status = api.statuses_lookup(ids, tweet_mode= "extended")
     empty_data = pd.DataFrame()
     for status in list_of_tw_status:
@@ -75,7 +82,11 @@ def fetch_tw(ids):
                      "text":status.full_text,
                      "date_time":status.created_at}
             empty_data = empty_data.append(tweet_elem, ignore_index = True)
-    empty_data.to_csv("tesla_tweets.csv", mode="a")
+    file_name = "tesla_tweets.csv"
+    if not os.path.isfile(file_name):
+        empty_data.to_csv(file_name)
+    else:    
+        empty_data.to_csv(file_name, mode="a", header=False)
     
     
 # process 50 entries at a time, bc original author couldn't loop through literally everything
@@ -88,5 +99,13 @@ for i in range(chunks):
     result = fetch_tw(batch)
     
     
-test = pd.read_csv("tesla_tweets.csv")
-    
+text = pd.read_csv("tesla_tweets.csv")
+
+
+# test4 = test.drop_duplicates()
+# len(test[test["date_time"]=="date_time"])
+# test2 = test.drop(test[test["date_time"]=="date_time"].index)
+# test.nunique()
+# test3 = test2.drop_duplicates()
+# sampleid = test2["id"][1]
+# test2[test2["id"]==sampleid]
